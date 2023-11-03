23bc0e9aaecbeea893d1e846df61c58d4af457d0
shahd mohamed <shahdelnassag@gmail.com>
2023-11-03 14:31:24 +0200

    First commit

---
+++ BigReal.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Q on 10/28/2023.
+//
+
+#include "BigReal.h"
+
+
+
---
+++ BigReal.h
@@ -0,0 +1,18 @@
+//
+// Created by Q on 10/28/2023.
+//
+
+#ifndef TASK_2_BIGREAL_H
+#define TASK_2_BIGREAL_H
+
+#include "string"
+#include "iostream"
+ using namespace std;
+
+
+
+
+
+
+
+#endif //TASK_2_BIGREAL_H
