261cd637f1489a88d2c9b2a4e06f0ab42c90e701
Hana <112332153+eng-hana74@users.noreply.github.com>
2023-11-05 20:43:57 +0200

    Update A2_Task2_S21_S22_20220533_20220116_20221192.cpp

--- A2_Task2_S21_S22_20220533_20220116_20221192.cpp
+++ A2_Task2_S21_S22_20220533_20220116_20221192.cpp
@@ -52,6 +52,41 @@ int main() {
     // Print the result
     cout << "Diff = ";
     diff.print();
+    //--------------------------------------------
+    // test oprator ==&=
+    BigReal x("+2.45446789646");
+    BigReal y("-3.8965690798");
+    if(x==y){
+        cout<<"x&y are equal\n";
+    }else{
+        cout<<"x&y are not equal\n";
+    }
+    x=y;
+    if(x==y){
+        cout<<"x&y are equal aftar = oprator\n";
+    }else{
+        cout<<"x&y are not equal aftar = oprator\n";
+    }
+    //--------------------------------------------
+    // test valid function
+    BigReal v("+4567.467987");
+    // constractor checks if nv is valid and outputs a massge that it's not valid big real
+    BigReal nv("+365487.467x.57x");
+    if(v.valid()){
+        cout<<" v is valid big real\n";
+    }else{
+        cout<<" v not valid big real\n";
+    }
+    if(nv.valid()){
+        cout<<" nv is valid big real\n";
+    }else{
+        cout<<" nv not valid big real\n";
+    }
+
+    
+    
+    
+    
 
 
     return 0;
